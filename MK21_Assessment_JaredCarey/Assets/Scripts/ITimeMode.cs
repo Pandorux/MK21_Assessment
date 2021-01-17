407690254b89a532ae18c4adcb4dac5210ad0596
Jared <itspandorux@gmail.com>
2021-01-17 19:56:03 +1000

    Created TimeMode Interface

---
+++ MK21_Assessment_JaredCarey/Assets/Scripts/ITimeMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface ITimeMode
+{
+    void StartTimeMode();
+    void ResetTimeMode();
+    void StopTimeMode();
+    void ValidateInputtedTime();
+    void TimeDisplayUpdated();
+    event EventHandler TimeDisplayUpdate;
+}
