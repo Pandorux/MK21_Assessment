34210de3fac1900cb15e94e0f59459547618ec06
Jared <itspandorux@gmail.com>
2021-01-17 21:54:50 +1000

    Refined ITimeMode and Created the TimeMode CurrentTimeTimeMode

---
+++ MK21_Assessment_JaredCarey/Assets/Scripts/CurrentTimeTimeMode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentTimeTimeMode : MonoBehaviour, ITimeMode
+{
+
+    private bool isTimeModeActive = false;
+
+    public event OnTimeUpdateEventHandler timeDisplayUpdate;
+    public void TimeDisplayUpdated(OnTimeUpdateEventArgs e)
+    {
+        if(timeDisplayUpdate != null)
+            timeDisplayUpdate(this, e);
+    }
+
+    public bool GetIsTimeModeActive()
+    {
+        return isTimeModeActive;
+    }
+
+    private void SetIsTimeModeActive(bool newState)
+    {
+        isTimeModeActive = newState;
+    }
+
+    public void StartTimeMode()
+    {
+        SetIsTimeModeActive(true);
+    }
+
+    public void ResetTimeMode()
+    {
+        SetIsTimeModeActive(false);
+    }
+
+    public void StopTimeMode()
+    {
+        SetIsTimeModeActive(false);
+    }
+
+    public void ValidateInputtedTime()
+    {
+        // TODO:
+        throw new NotImplementedException();
+    }
+    
+}
--- MK21_Assessment_JaredCarey/Assets/Scripts/ITimeMode.cs
+++ MK21_Assessment_JaredCarey/Assets/Scripts/ITimeMode.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 
 public interface ITimeMode
 {
+    bool GetIsTimeModeActive();
     void StartTimeMode();
     void ResetTimeMode();
     void StopTimeMode();
     void ValidateInputtedTime();
-    void TimeDisplayUpdated();
-    event EventHandler TimeDisplayUpdate;
+    void TimeDisplayUpdated(OnTimeUpdateEventArgs e);
+    event OnTimeUpdateEventHandler timeDisplayUpdate;
+}
+
+public delegate void OnTimeUpdateEventHandler(object sender, OnTimeUpdateEventArgs e);
+public class OnTimeUpdateEventArgs : EventArgs
+{
+    public string time;
 }
