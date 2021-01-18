ec3d08bab498a67f2b224cb80afe40e5ee93f642
Jared <itspandorux@gmail.com>
2021-01-18 19:53:01 +1000

    Implemented ITimeMode Interface in new Abstract Class AbstractTimeMode

---
+++ MK21_Assessment_JaredCarey/Assets/Scripts/AbstractTimeMode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractTimeMode : MonoBehaviour, ITimeMode
+{
+    protected bool isTimeModeActive = false;
+
+    public event OnTimeUpdateEventHandler timeDisplayUpdate;
+    public void TimeDisplayUpdated(OnTimeUpdateEventArgs e)
+    {
+        if(timeDisplayUpdate != null)
+            timeDisplayUpdate(this, e);
+    }
+
+    public bool GetIsTimeModeActive()
+    {
+        return isTimeModeActive;
+    }
+
+    private void SetIsTimeModeActive(bool newState)
+    {
+        isTimeModeActive = newState;
+    }
+
+    public virtual void StartTimeMode()
+    {
+        SetIsTimeModeActive(true);
+    }
+
+    public virtual void ResetTimeMode()
+    {
+        SetIsTimeModeActive(false);
+    }
+
+    public virtual void StopTimeMode()
+    {
+        SetIsTimeModeActive(false);
+    }
+
+    public virtual void ValidateInputtedTime()
+    {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}
--- MK21_Assessment_JaredCarey/Assets/Scripts/CurrentTimeTimeMode.cs
+++ MK21_Assessment_JaredCarey/Assets/Scripts/CurrentTimeTimeMode.cs
@@ -3,62 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurrentTimeTimeMode : MonoBehaviour, ITimeMode
+public class CurrentTimeTimeMode : AbstractTimeMode
 {
-
-    private bool isTimeModeActive = false;
-
-    public event OnTimeUpdateEventHandler timeDisplayUpdate;
-    public void TimeDisplayUpdated(OnTimeUpdateEventArgs e)
-    {
-        if(timeDisplayUpdate != null)
-            timeDisplayUpdate(this, e);
-    }
-
-    void Start()
-    {
-        StartTimeMode();
-    }
-
-    void Update()
-    {
-        if(GetIsTimeModeActive())
-        {
-            UpdateCurrentTime();
-        }
-    }
-
-    public bool GetIsTimeModeActive()
-    {
-        return isTimeModeActive;
-    }
-
-    private void SetIsTimeModeActive(bool newState)
-    {
-        isTimeModeActive = newState;
-    }
-
-    public void StartTimeMode()
-    {
-        SetIsTimeModeActive(true);
-    }
-
-    public void ResetTimeMode()
-    {
-        SetIsTimeModeActive(false);
-    }
-
-    public void StopTimeMode()
-    {
-        SetIsTimeModeActive(false);
-    }
-
-    public void ValidateInputtedTime()
-    {
-        // TODO:
-        throw new NotImplementedException();
-    }
-    
     public void UpdateCurrentTime()
     {
         OnTimeUpdateEventArgs e = new OnTimeUpdateEventArgs();
